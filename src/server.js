54e9061300b301d01fcb59932b3ff432a521041c
GwangrokBaek <zester926@gmail.com>
2022-12-02 21:56:30 +0900

    webRTC 기반 영상통화 기능 구현

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import { Server } from "socket.io";
+import SocketIO from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
@@ -11,80 +11,25 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
-
-instrument(io, {
-  auth: false,
-});
-
-function getUserCountsOfRoom(roomName) {
-  return io.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-function getPublicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = io;
-
-  const publicRooms = [];
-
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-
-  return publicRooms;
-}
+const io = SocketIO(server);
 
 io.on("connection", socket => {
-  socket["nickname"] = "Anon";
-  io.sockets.emit("room_change", getPublicRooms());
+  socket.on("join_room", room_name => {
+    socket.join(room_name);
 
-  socket.onAny(event => {
-    console.log(`Socket Event: ${event}`);
+    socket.to(room_name).emit("welcome");
   });
-
-  socket.on("enter_room", (roomName, callback) => {
-    console.log(roomName);
-    socket.join(roomName.payload);
-    callback(getUserCountsOfRoom(roomName.payload));
-
-    socket
-      .to(roomName.payload)
-      .emit("welcome", socket.nickname, getUserCountsOfRoom(roomName.payload));
-    io.sockets.emit("room_change", getPublicRooms());
+  socket.on("offer", (offer, room_name) => {
+    socket.to(room_name).emit("offer", offer);
   });
-
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach(room =>
-      socket
-        .to(room)
-        .emit("bye", socket.nickname, getUserCountsOfRoom(room) - 1)
-    );
-    io.sockets.emit("room_change", getPublicRooms());
+  socket.on("answer", (answer, room_name) => {
+    socket.to(room_name).emit("answer", answer);
   });
-
-  socket.on("disconnect", () => {
-    io.sockets.emit("room_change", getPublicRooms());
+  socket.on("ice", (ice, room_name) => {
+    socket.to(room_name).emit("ice", ice);
   });
-
-  socket.on("new_message", (message, roomName, callback) => {
-    socket.to(roomName).emit("new_message", `${socket.nickname}: ${message}`);
-    callback();
-  });
-
-  socket.on("nickname", nickname => (socket["nickname"] = nickname));
 });
 
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 server.listen(3000, handleListen);
